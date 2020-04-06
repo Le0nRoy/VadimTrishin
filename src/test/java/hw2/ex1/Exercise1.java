7b31fff61e5b85edb0cbd73ef122eed8aa2a9c18
le0n <cerber9283@gmail,com>
2020-04-06 15:41:45 +0300

    Replaced tesng.xml files for hw1 in separate directory. Created testng.xml file for hw2.ex1. Renamed first test in Exercise1 and reduced sleep time for it.

--- src/test/java/hw2/ex1/Exercise1.java
+++ src/test/java/hw2/ex1/Exercise1.java
@@ -8,7 +8,7 @@ import static org.testng.Assert.assertEquals;
 public class Exercise1 extends BaseClass {
 
     @Test
-    public void openSiteByURLTest() {
+    public void openSiteByURLAndCheckItsTitleTest() {
         chromeDriver.get("https://jdi-testing.github.io/jdi-light/index.html");
         String ret = chromeDriver.getTitle();
         String expected = "Home Page";
@@ -16,7 +16,7 @@ public class Exercise1 extends BaseClass {
 
         // TODO replace with WebDriver wait methods (is it needed at all?)
         try {
-            Thread.sleep(10000);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
