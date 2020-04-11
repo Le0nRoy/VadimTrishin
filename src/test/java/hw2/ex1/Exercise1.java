aa993ada745b4d81017167b712a8c4b636d3bbd3
le0n <cerber9283@gmail,com>
2020-04-12 02:14:51 +0300

    Added several tests for Exercise1. Prepared functions for Exercise2.

--- src/test/java/hw2/ex1/Exercise1.java
+++ src/test/java/hw2/ex1/Exercise1.java
@@ -3,13 +3,103 @@ package hw2.ex1;
 import hw2.BaseClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.testng.Assert.*;
 
 public class Exercise1 extends BaseClass {
+
+    @Test(enabled = false)
+//    @Test
+    private void checkItemsOnHeader() {
+
+        List<WebElement> elements = chromeDriver.findElements(
+                By.xpath("//ul[@class='uui-navigation nav navbar-nav m-l8']/li"));
+
+        int expectedNumOfElements = 4;
+        List<String> expectedNames = new ArrayList<String>(Arrays.asList(
+                "Home",
+                "Contact form",
+                "Service",
+                "Metals & Colors"
+        ));
+
+        assertEquals(elements.size(), expectedNumOfElements);
+        assertNotSame(elements, expectedNames);
+    }
+
+    @Test(enabled = false)
+//    @Test
+    private void checkImagesOnIndexPage() {
+
+        List<WebElement> elements = chromeDriver.findElements(By.className("benefit-icon"));
+
+        int expectedNumOfImages = 4;
+
+        assertEquals(elements.size(), expectedNumOfImages);
+    }
+
+    @Test(enabled = false)
+//    @Test
+    private void checkTextsOnIndexPage() {
+
+        List<WebElement> elements = chromeDriver.findElements(By.className("benefit-txt"));
+
+        int expectedNumOfTexts = 4;
+
+        assertEquals(elements.size(), expectedNumOfTexts);
+    }
+
+    @Test(enabled = false)
+//    @Test
+    private void checkExistanceOfIFrame() {
+
+        WebElement frameButton = chromeDriver.findElement(By.xpath("//input[@id='frame-button']"));
+//        WebElement frameButton = chromeDriver.findElement(By.xpath("//input[@value='Frame Button']"));
+//        WebElement frameButton = chromeDriver.findElement(By.xpath("//iframe//*[@id='frame-button']"));
+//        WebElement frameButton = chromeDriver.findElement(By.xpath("//iframe//*[@value='Frame Button']"));
+//        List<WebElement> elements = chromeDriver.findElements(By.tagName("iframe"));
+
+//        int numOfElements = elements.size();
+//        int numOfExceptions = 0;
+//        for (WebElement el : elements) {
+////            el.findElement(By.xpath("//input[@id='frame-button']"));
+//            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='frame-button']")));
+//        }
+//        assertNotEquals(numOfExceptions, numOfElements);
+    }
+
+    @Test(enabled = false)
+//    @Test
+    private void switchToIFrameAndCheckItsContents() {
+
+    }
+
+    @Test(enabled = false)
+//    @Test
+    private void checkItemsOnLeftSection() {
+
+        List<WebElement> elements = chromeDriver.findElements(By.xpath("//ul[@class='sidebar-menu']/li/a/span"));
+
+        int expectedNumOfElements = 5;
+        List<String> expectedNames = new ArrayList<String>(Arrays.asList(
+                "Home",
+                "Contact form",
+                "Service",
+                "Metals & Colors",
+                "Elements packs"
+        ));
+
+        assertEquals(elements.size(), expectedNumOfElements);
+        assertNotSame(elements, expectedNames);
+    }
 
     // FIXME delete after debugging
     @Deprecated
--- src/test/java/hw2/ex2/Exercise2.java
+++ src/test/java/hw2/ex2/Exercise2.java
@@ -1,7 +1,37 @@
 package hw2.ex2;
 
 import hw2.BaseClass;
+import org.testng.annotations.Test;
 
 public class Exercise2 extends BaseClass {
+
+    @Test(enabled = false)
+//    @Test
+    private void selectCheckboxesAndAssertTheirStatuses() {
+
+    }
+
+    @Test(enabled = false)
+//    @Test
+    private void selectRadiobuttonsAndAssertTheirStatusses() {
+
+    }
+
+    @Test(enabled = false)
+//    @Test
+    private void selectInDropdownAndAssertItsStatus() {
+
+    }
+
+    // FIXME delete after debugging
+    @Deprecated
+    private void sleep() {
+
+        try {
+            Thread.sleep(WAIT_TIMEOUT);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
