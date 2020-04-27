72e97e25d19f6380d24cad503cc65ba8728a3397
le0n <cerber9283@gmail,com>
2020-04-27 18:55:23 +0300

    Added properties and set up JDI

--- src/test/java/hw/jdi/BaseClass.java
+++ src/test/java/hw/jdi/BaseClass.java
@@ -1,9 +1,32 @@
 package hw.jdi;
 
+import hw.jdi.site.EpamSite;
+import hw.jdi.site.HomePage;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static com.epam.jdi.light.driver.WebDriverUtils.killAllSeleniumDrivers;
+import static com.epam.jdi.light.elements.init.PageFactory.initElements;
+
 public class BaseClass {
 
+//    private EpamSite epamSite;
+
+    @BeforeMethod(alwaysRun = true)
+    public void beforeTestMethod() {
+
+        initElements(EpamSite.class);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void afterTestMethod() {
+        killAllSeleniumDrivers();
+    }
+
+    @Test
     public void loginOnSiteAndFillFormsTest() {
 
-
+        EpamSite.open();
     }
 }
--- src/test/java/hw/jdi/site/HomePage.java
+++ src/test/java/hw/jdi/site/HomePage.java
@@ -3,7 +3,9 @@ package hw.jdi.site;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.Url;
 
-@Url("/index.html")
 public class HomePage extends WebPage {
+
+    private LoginForm loginForm;
+
 
 }
