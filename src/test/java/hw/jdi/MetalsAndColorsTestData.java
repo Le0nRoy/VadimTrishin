bfc85a78b39e3df5d9cc1f5d3811869daf148e24
le0n <cerber9283@gmail,com>
2020-04-29 20:18:31 +0300

    Added lombok for test data

--- src/test/java/hw/jdi/MetalsAndColorsTestData.java
+++ src/test/java/hw/jdi/MetalsAndColorsTestData.java
@@ -1,7 +1,12 @@
 package hw.jdi;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
 public class MetalsAndColorsTestData {
 
     private List<Integer> summary;
@@ -9,36 +14,6 @@ public class MetalsAndColorsTestData {
     private String color;
     private String metals;
     private List<String> vegetables;
-
-    public MetalsAndColorsTestData(List<Integer> summary, List<String> elements, String color, String metal, List<String> vegetables) {
-
-        this.summary = summary;
-        this.elements = elements;
-        this.color = color;
-        this.metals = metal;
-        this.vegetables = vegetables;
-    }
-
-    public List<Integer> getSummary() {
-
-        return summary;
-    }
-    public List<String> getElements() {
-
-        return elements;
-    }
-    public String getColor() {
-
-        return color;
-    }
-    public String getMetals() {
-
-        return metals;
-    }
-    public List<String> getVegetables() {
-
-        return vegetables;
-    }
 
     @Override
     public String toString() {
