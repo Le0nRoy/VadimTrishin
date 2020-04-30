bd0ba17316bbe38c1607a9df7299d9b5aaeee788
le0n <cerber9283@gmail,com>
2020-04-30 19:29:31 +0300

    Refactored Exercise2 to Step Pattern

--- src/test/java/hw5/ex2/Exercise2.java
+++ src/test/java/hw5/ex2/Exercise2.java
@@ -1,14 +1,13 @@
 package hw5.ex2;
 
 import hw5.AbstractBaseTest;
-import hw5.site.DifferentElementsPage;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Exercise2 extends AbstractBaseTest {
 
@@ -25,62 +24,43 @@ public class Exercise2 extends AbstractBaseTest {
     @Parameters({"userName", "password"})
     private void exerciseTest(String userName, String password) {
 
+        String expected;
+        List<String> expectedOptionsCheckboxes;
+        String expectedOptionRadiobutton;
+        String expectedOptionColor;
+
         // Tasks 1 - 2
         steps.openSiteByURL(TEST_SITE_URL);
-        String expected = "Home Page";
+        expected = "Home Page";
         steps.siteTitleShouldBe(expected);
 
         // Tasks 3 - 4
         steps.login(userName, password);
         expected = "ROMAN IOVLEV";
         steps.usernameShouldBe(expected);
-//
-//        // Task 5
-//        DifferentElementsPage page = PageFactory.initElements(chromeDriver, DifferentElementsPage.class);
-//        page.open();
-//        DifferentElementsPage.SelectableElementComponent checkboxObject = page.getCheckboxesObject();
-//        DifferentElementsPage.SelectableElementComponent radioButtonsObject = page.getRadioButtonsObject();
-//        DifferentElementsPage.SelectableElementComponent colorsSelectObject = page.getColorsSelectObject();
-//        DifferentElementsPage.InfoPanelObject infoPanelObject = page.getInfoPanelObject();
-//
-//        // Task 6
-//        ArrayList<String> expectedOptionsCheckboxes = new ArrayList<String>(Arrays.asList(
-//                "Water",
-//                "Wind"
-//        ));
-//        for (String option : expectedOptionsCheckboxes) {
-//            checkboxObject.selectElement(option);
-//        }
-//
-//        // Task 7
-//        ArrayList<String> expectedOptionsRadioButtons = new ArrayList<String>(Arrays.asList(
-//                "Selen"
-//        ));
-//        for (String option : expectedOptionsRadioButtons) {
-//            radioButtonsObject.selectElement(option);
-//        }
-//
-//        // Task 8
-//        ArrayList<String> expectedOptionsSelectables = new ArrayList<String>(Arrays.asList(
-//                "Yellow"
-//        ));
-//        for (String option : expectedOptionsSelectables) {
-//            colorsSelectObject.selectElement(option);
-//        }
-//
-//        // Task 9
-//        for (String expectedOption : expectedOptionsCheckboxes) {
-//            softAssert.assertTrue(infoPanelObject.findStringInInfoPanelByTwoPatterns(expectedOption, "condition changed to true"));
-//            softAssert.assertTrue(checkboxObject.isElementSelected(expectedOption));
-//        }
-//        for (String expectedOption : expectedOptionsRadioButtons) {
-//            softAssert.assertTrue(infoPanelObject.findStringInInfoPanelByTwoPatterns(expectedOption, "metal"));
-//            softAssert.assertTrue(radioButtonsObject.isElementSelected(expectedOption));
-//        }
-//        for (String expectedOption : expectedOptionsSelectables) {
-//            softAssert.assertTrue(infoPanelObject.findStringInInfoPanelByTwoPatterns(expectedOption, "Colors"));
-//            softAssert.assertTrue(colorsSelectObject.isElementSelected(expectedOption));
-//        }
+
+        // Task 5
+        steps.openDifferentElementsPage();
+
+        // Task 6
+        expectedOptionsCheckboxes = new ArrayList<String>(Arrays.asList(
+                "Water",
+                "Wind"
+        ));
+        steps.selectCheckboxOptions(expectedOptionsCheckboxes);
+
+        // Task 7
+        expectedOptionRadiobutton = "Selen";
+        steps.selectRadiobutton(expectedOptionRadiobutton);
+
+        // Task 8
+        expectedOptionColor = "Yellow";
+        steps.selectColor(expectedOptionColor);
+
+        // Task 9
+        steps.checkboxesAreSelected(expectedOptionsCheckboxes);
+        steps.radiobuttonIsSelected(expectedOptionRadiobutton);
+        steps.colorIsSelected(expectedOptionColor);
     }
 
 }
--- src/test/java/hw5/ex2/Exercise2Steps.java
+++ src/test/java/hw5/ex2/Exercise2Steps.java
@@ -1,7 +1,69 @@
 package hw5.ex2;
 
 import hw5.AbstractSteps;
+import hw5.site.DifferentElementsPage;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class Exercise2Steps extends AbstractSteps {
+
+    private DifferentElementsPage page;
+    private DifferentElementsPage.SelectableElementComponent checkboxObject;
+    private DifferentElementsPage.SelectableElementComponent radiobuttonObject;
+    private DifferentElementsPage.SelectableElementComponent colorsSelectObject;
+    private DifferentElementsPage.InfoPanelObject infoPanelObject;
+
+    Exercise2Steps() {
+
+        super();
+        page = PageFactory.initElements(chromeDriver, DifferentElementsPage.class);
+        checkboxObject = page.getCheckboxesObject();
+        radiobuttonObject = page.getRadioButtonsObject();
+        colorsSelectObject = page.getColorsSelectObject();
+        infoPanelObject = page.getInfoPanelObject();
+    }
+
+    public void openDifferentElementsPage() {
+
+        page.open();
+    }
+
+    public void selectCheckboxOptions(List<String> options) {
+
+        for (String option : options) {
+            checkboxObject.selectElement(option);
+        }
+    }
+
+    public void selectRadiobutton(String option) {
+
+        radiobuttonObject.selectElement(option);
+    }
+
+    public void selectColor(String option) {
+
+        colorsSelectObject.selectElement(option);
+    }
+
+    public void checkboxesAreSelected(List<String> expectedOptions) {
+
+        for (String expectedOption : expectedOptions) {
+            softAssert.assertTrue(infoPanelObject.findStringInInfoPanelByTwoPatterns(expectedOption, "condition changed to true"));
+            softAssert.assertTrue(checkboxObject.isElementSelected(expectedOption));
+        }
+    }
+
+    public void radiobuttonIsSelected(String expectedOption) {
+
+        softAssert.assertTrue(infoPanelObject.findStringInInfoPanelByTwoPatterns(expectedOption, "metal"));
+        softAssert.assertTrue(radiobuttonObject.isElementSelected(expectedOption));
+    }
+
+    public void colorIsSelected(String expectedOption){
+
+        softAssert.assertTrue(infoPanelObject.findStringInInfoPanelByTwoPatterns(expectedOption, "Colors"));
+        softAssert.assertTrue(colorsSelectObject.isElementSelected(expectedOption));
+    }
 
 }
